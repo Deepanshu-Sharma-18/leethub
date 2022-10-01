4e8696f79f462682db2606256cc1238a6c82dea7
Deepanshu-Sharma-18 <67851435+Deepanshu-Sharma-18@users.noreply.github.com>
2022-10-01 11:55:18 +0530

    Time: 6 ms (53.48%), Space: 7.9 MB (34.05%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> s;
+        for(int i=1;i<=n;i++){
+            
+            if( i % 3 == 0 && i % 5 == 0){
+                s.push_back("FizzBuzz");
+                continue;
+            }
+             if( i % 3 == 0){
+                s.push_back("Fizz");
+                 continue;
+            }
+            if( i % 5 == 0 ){
+                s.push_back("Buzz");
+                continue;
+            }
+
+            
+            s.push_back(to_string(i));
+        }
+        
+        return s;
+    }
+};
