cdfb6f61f450f479977efa982a18e71cbcc4f0a9
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2023-08-13 11:28:50 +0530

    Time: 1202 ms (25.14%) | Memory: 191.6 MB (7.19%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -3,29 +3,27 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
-       map<int,int> mp;
+      
        set<vector<int>> st;
-       vector<int>v;
+ 
+
+
+       sort(nums.begin(),nums.end());
         
 
         for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(mp.find(0 - nums[i]-nums[j]) != mp.end()){
-                    
-              
-                    if(i != j && j!=mp[ - nums[i]-nums[j]] && i!=mp[ - nums[i]-nums[j]]){
-                        v.push_back(nums[i]);
-                        v.push_back(nums[j]);
-                        v.push_back(-nums[i]-nums[j]);
-                        sort(v.begin(),v.end());
-                        st.insert(v);
-                        v.clear();
-                         
-                    }
-                     
-                }
-                mp[nums[j]] = j;
-            }
+            int j = i+1 , k = nums.size()-1;
+           while(j < k){
+               if(nums[i] + nums[j] + nums[k] == 0){
+                   st.insert({nums[i],nums[j],nums[k]});
+                   j++;
+                   k--;
+               }else if(nums[i] + nums[j] + nums[k] < 0){
+                   j++;
+               }else{
+                   k--;
+               }
+           }
             
         }
 
