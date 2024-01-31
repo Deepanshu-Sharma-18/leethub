a090cd9038ab08b8feb7b9d2655d61f9e1ac1178
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2024-01-31 22:03:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (20.99%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -13,28 +13,54 @@ class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
 
-        vector<int> ans;
+         vector<int> ans;
         
-        stack<TreeNode*> nodes;
+        // stack<TreeNode*> nodes;
 
-        TreeNode* temp = root;
-        while(temp != NULL){
-            nodes.push(temp );
-            temp = temp->left;
-        }
+        // TreeNode* temp = root;
+        // while(temp != NULL){
+        //     nodes.push(temp );
+        //     temp = temp->left;
+        // }
 
-        while(nodes.size() != 0){
-            temp =  nodes.top();
-            ans.push_back(temp->val);
-            nodes.pop();
+        // while(nodes.size() != 0){
+        //     temp =  nodes.top();
+        //     ans.push_back(temp->val);
+        //     nodes.pop();
 
-            if(temp->right != NULL){
-                temp = temp->right;
-                while(temp != NULL){
-                    nodes.push(temp);
-                    temp = temp->left;
+        //     if(temp->right != NULL){
+        //         temp = temp->right;
+        //         while(temp != NULL){
+        //             nodes.push(temp);
+        //             temp = temp->left;
+        //         }
+        //     }
+        // }
+
+        //morris
+
+        while(root){
+            if(root->left == NULL){
+                ans.push_back(root->val);
+                root = root->right;
+            }else{
+
+                TreeNode* pre = root->left;
+
+                while(pre->right && pre->right != root){
+                    pre = pre->right;
+                }
+
+                if(pre->right == NULL){
+                    pre->right = root;
+                    root = root->left;
+                }else{
+                    pre->right = NULL;
+                    ans.push_back(root->val);
+                    root = root->right;
                 }
             }
+
         }
 
         return ans;
