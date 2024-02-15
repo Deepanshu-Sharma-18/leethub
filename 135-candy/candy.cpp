64067f4d470cfc4a5a7ba6b33d47afcbb5a005be
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2024-02-15 19:27:24 +0530

    Time: 63 ms (5.00%) | Memory: 27.4 MB (5.09%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int num = 0;
+        map<int,int> mp;
+
+        for(int i=1;i<ratings.size();i++){
+           
+                if(ratings[i-1] < ratings[i]){
+                    mp[i] = mp[i-1]+ 1;
+                }
+        }
+
+        for(int i=ratings.size()-1;i>0;i--){
+            
+                if(ratings[i-1] > ratings[i]){
+                    mp[i-1] = max(mp[i] + 1 , mp[i-1] ) ;
+                }
+            
+        }
+
+        for(int i=0;i<ratings.size();i++){
+            num += mp[i];
+        }
+
+        return num + ratings.size();
+    }
+};
+
+
+
+//first and last -- sirf ek neighbour consider
+
+//middle mai 2 neighbours
