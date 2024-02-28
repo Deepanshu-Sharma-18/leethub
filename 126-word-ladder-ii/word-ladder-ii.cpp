4f71aa3ceaefba0a3c8fb89559b283323e0145a6
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2024-02-28 21:01:33 +0530

    Time: 8 ms (89.03%) | Memory: 12.5 MB (56.29%) - LeetSync

---
+++ 126-word-ladder-ii/word-ladder-ii.cpp
@@ -0,0 +1,102 @@
+class Solution {
+    unordered_map<string,int> mpp;
+    vector<vector<string>> ans;
+    string b;
+
+    void dfs(string word, vector<string> &seq)
+    {
+        
+        if (word == b)
+        {
+            reverse(seq.begin(), seq.end());
+            ans.push_back(seq);
+    
+      
+            reverse(seq.begin(), seq.end());
+            return;
+        }
+        int sz = word.size();
+        int steps = mpp[word];
+
+      
+        for (int i = 0; i < sz; i++)
+        {
+            char original = word[i];
+            for (char ch = 'a'; ch <= 'z'; ch++)
+            {
+                word[i] = ch;
+                if (mpp.find(word) != mpp.end() && mpp[word] + 1 == steps)
+                {
+                    seq.push_back(word);
+                    dfs(word, seq);
+                    
+                    seq.pop_back();
+                }
+            }
+            word[i] = original;
+        }
+    }
+
+
+public:
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+
+        unordered_set<string> st(wordList.begin(), wordList.end());
+
+        // Perform BFS traversal and push the string in the queue
+        // as soon as they’re found in the wordList.
+        queue<string> q;
+        b = beginWord;
+        q.push({beginWord});
+
+        // beginWord initialised with level 1.
+        mpp[beginWord] = 1;
+        int sz = beginWord.size();
+        st.erase(beginWord);
+        while (!q.empty())
+        {
+
+            string word = q.front();
+            int steps = mpp[word];
+            q.pop();
+            
+            // Break out if the word matches the endWord
+            if (word == endWord)
+                break;
+
+            // Replace each character of the word with letters from a-z 
+            // and check whether the transformed word is present in the 
+            // wordList or not, if yes then push to queue
+            for (int i = 0; i < sz; i++)
+            {
+                char original = word[i];
+
+                for (char ch = 'a'; ch <= 'z'; ch++)
+                {
+
+                    word[i] = ch;
+                    if (st.count(word))
+                    {
+                        q.push(word);
+                        st.erase(word);
+                       
+                        // push the word along with its level
+                        // in the map data structure.
+                        mpp[word] = steps + 1;
+                    }
+                }
+                word[i] = original;
+            }
+        }
+    
+        // If we reach the endWord, we stop and move to step-2
+        // that is to perform reverse dfs traversal.
+        if (mpp.find(endWord) != mpp.end())
+        {
+            vector<string> seq;
+            seq.push_back(endWord);
+            dfs(endWord, seq);
+        }
+        return ans;
+    }
+};
