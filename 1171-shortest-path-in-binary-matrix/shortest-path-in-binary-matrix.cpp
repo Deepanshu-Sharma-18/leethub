560e5f0ec484c3b2a6aea0d9aa2a1c99ecb54a4a
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2024-03-05 19:37:56 +0530

    Time: 90 ms (24.85%) | Memory: 42.6 MB (16.16%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        if(grid[0][0] == 1) return -1;
+        if(grid[0][0] == 1 || grid[n-1][n-1]) return -1;
         if(n == 1) return 1;
         
         queue<vector<int>> pq;
         vector<vector<int>> dist(n,vector<int>(n,1e9));
-
-        vector<pair<int,int>> adj = {{0,1} , {0,-1} , {1,0} ,{-1,0} , {-1,-1} , {1,1} , {1,-1} , {-1,1}};
         
 
         pq.push({0,0,0});
@@ -18,18 +16,20 @@ public:
             auto s = pq.front();
             pq.pop();
 
-            for(auto it : adj){
-                int row = it.first + s[1];
-                int col = it.second + s[2];
+            for(int i=-1;i<=1;i++){
+                for(int j=-1;j<=1;j++){
+                    int row = i + s[1];
+                    int col = j + s[2];
 
 
-                if(row >= 0 && row < n && col >= 0 && col < n && grid[row][col] == 0 && s[0] + 1 < dist[row][col]){
-                    if(row == n-1 && col == n-1 ){
-                        return s[0] + 2;
+                    if(row >= 0 && row < n && col >= 0 && col < n && grid[row][col] == 0 && s[0] + 1 < dist[row][col]){
+                        if(row == n-1 && col == n-1 ){
+                            return s[0] + 2;
+                        }
+
+                        pq.push({s[0] + 1 , row,col});
+                        dist[row][col] = s[0] + 1;
                     }
-
-                    pq.push({s[0] + 1 , row,col});
-                    dist[row][col] = s[0] + 1;
                 }
             }
 
