c9ad5166a132008d704081f9be29c0b558d1e9ac
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2024-03-19 11:59:43 +0530

    Time: 95 ms (42.81%) | Memory: 46.2 MB (52.92%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,75 @@
+class DisjointSet {
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DisjointSet ds(n);
+
+        int extraedge = 0;
+
+        for(auto it : connections){
+            if(ds.findUPar(it[0]) == ds.findUPar(it[1])){
+                extraedge++;
+            }else{
+                ds.unionBySize(it[0] , it[1]);
+            }
+        }
+
+        int components = 0;
+
+        for(int i=0;i<n;i++){
+            if(ds.parent[i] == i) components++;
+        }
+
+        if(components - 1 <= extraedge) return components - 1;
+        return -1;
+    }
+};
