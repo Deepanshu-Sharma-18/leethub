2242d598bd8241f966ed9197019133c282a75c79
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2024-05-04 11:59:32 +0530

    Time: 4 ms (13.07%) | Memory: 7.6 MB (45.68%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -19,9 +19,30 @@ public:
         return dp[m][n] =  up + down;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m , vector<int>(n,-1));
 
-        return f(m-1,n-1,dp);
+        vector<vector<int>> dp(m , vector<int>(n,0));
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i == 0 && j==0 ) dp[i][j] = 1;
+
+                else{
+
+                    int up = 0;
+                    int down = 0;
+
+                    if(i > 0)
+                    up = dp[i-1][j];
+                    if(j > 0) 
+                    down = dp[i][j-1];
+
+                    dp[i][j] = up + down;
+                }
+
+            }
+        }
+
+        return dp[m-1][n-1];
         
     }
 };
