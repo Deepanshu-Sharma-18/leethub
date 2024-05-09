a1bd85c89803b2fc62298f58fa05bbdf57f321b1
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2024-05-09 21:06:09 +0530

    Time: 35 ms (48.86%) | Memory: 20.2 MB (36.99%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -31,27 +31,30 @@ public:
     }
 
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size() , vector<int>(amount + 1 , 0));
+        vector<int> prev(amount + 1, 0);
+        vector<int> cur(amount + 1, 0);
 
         for(int i=0;i<=amount;i++){
-            if(i %  coins[0] == 0 ) dp[0][i] = i / coins[0];
-            else dp[0][i] = 1e9;
+            if(i %  coins[0] == 0 ) prev[i] = i / coins[0];
+            else prev[i] = 1e9;
         }
 
         for(int i=1;i<coins.size();i++){
             for(int j = 0;j<=amount;j++){
-                int notpick = dp[i-1][j];
+                int notpick = prev[j];
 
                 int pick = 1e9;
-                if(coins[i] <= j) pick = 1 + dp[i][j - coins[i]];
+                if(coins[i] <= j) pick = 1 + cur[j - coins[i]];
                 
-                dp[i][j] = min(pick , notpick);
+                cur[j] = min(pick , notpick);
                 
             }
+
+            prev = cur;
         }
 
 
-        if(dp[coins.size()-1][amount] >= 1e9) return -1;
-        return dp[coins.size()-1][amount];
+        if(prev[amount] >= 1e9) return -1;
+        return prev[amount];
     }
 };
