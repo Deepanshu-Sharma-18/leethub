769b4e7c9c12e026650250d6a4711d1af96e9a90
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2024-06-25 09:38:52 +0530

    Time: 4 ms (94.68%) | Memory: 12.7 MB (83.20%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,3 +1,4 @@
+
 class Solution {
 public:
     int f(vector<int>& nums , int ind, int prev , int n,vector<vector<int>> &dp){
@@ -15,13 +16,32 @@ public:
 
         return dp[ind][prev+1] = max(a,b);
     }
+
+
     int lengthOfLIS(vector<int>& nums) {
-        int ans = 1, n = size(nums);
-        vector<int> dp(n, 1);
-        for(int i = 0; i < n; i++) 
-            for(int j = 0; j < i; j++) 
-                if(nums[i] > nums[j]) 
-				    dp[i] = max(dp[i], dp[j] + 1), ans = max(ans, dp[i]);
-        return ans;
+        // int ans = 1, n = size(nums);
+        // vector<int> dp(n, 1);
+        // for(int i = 0; i < n; i++) 
+        //     for(int j = 0; j < i; j++) 
+        //         if(nums[i] > nums[j]) 
+		// 		    dp[i] = max(dp[i], dp[j] + 1),
+        //             ans = max(ans, dp[i]);
+        // return ans;
+
+        vector<int> temp;
+        temp.push_back(nums[0]);
+
+        int len = 1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i] > temp.back()){
+                temp.push_back(nums[i]);
+                len++;
+            }else{
+                int ind = std::lower_bound(temp.begin() , temp.end() , nums[i]) - temp.begin();
+                temp[ind] = nums[i];
+            }
+        }
+
+        return len;
     }
 };
