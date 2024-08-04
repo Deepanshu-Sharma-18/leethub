7e54128b99adda06276f87c611bd9e26f05d34c0
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2024-08-04 12:44:15 +0530

    Time: 348 ms (16.84%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,8 +1,7 @@
-# Write your MySQL query statement below
-# Write your MySQL query statement below
-with base as(select requester_id id from RequestAccepted
-union all
-select accepter_id id from RequestAccepted)
-
-
-select id, count(*) num  from base group by 1 order by 2 desc limit 1
+select id, count(*) num from 
+(
+      (select requester_id id from RequestAccepted) 
+      union all 
+      (select accepter_id id from RequestAccepted)
+) tb 
+group by id order by num desc limit 1
