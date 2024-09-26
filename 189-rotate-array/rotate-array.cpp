3225e2ec977fd9c6da06938949d47b26808d3617
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2024-09-26 10:14:01 +0530

    Time: 19 ms (69.02%) | Memory: 27.6 MB (51.24%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,27 +4,47 @@ public:
         int n = nums.size();
         k = k%n;
 
-        vector<int> temp;
+        // vector<int> temp;
 
-        int ind = n-1;
+        // int ind = n-1;
 
-        for(int i = 0 ;i < k;i++){
-            temp.push_back(nums[ind]);
-            ind--;
+        // for(int i = 0 ;i < k;i++){
+        //     temp.push_back(nums[ind]);
+        //     ind--;
+        // }
+
+        //  ind = n-1;
+
+        // for(int i=n-1-k; i >=0  ; i--){
+        //     swap(nums[ind] , nums[i]);
+        //     ind--;
+        // }
+
+        // ind = 0;
+
+        // for(int i=temp.size()-1; i>=0; i--){
+        //     nums[ind] = temp[i];
+        //     ind++;
+        // }
+
+
+        reverse(nums.begin() , nums.end());
+
+        int i=0 , j = k-1;
+
+        while(i < j){
+            swap(nums[i] , nums[j]);
+            i++;
+            j--;
         }
 
-         ind = n-1;
+        i = k;
+        j = n-1;
 
-        for(int i=n-1-k; i >=0  ; i--){
-            swap(nums[ind] , nums[i]);
-            ind--;
-        }
-
-        ind = 0;
-
-        for(int i=temp.size()-1; i>=0; i--){
-            nums[ind] = temp[i];
-            ind++;
+        while(i < j){
+            swap(nums[i], nums[j]);
+            i++;
+            j--;
         }
 
 
