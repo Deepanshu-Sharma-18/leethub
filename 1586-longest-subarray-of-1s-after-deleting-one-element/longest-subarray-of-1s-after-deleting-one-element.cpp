7495970494f4a76b09c71b2bcfa84cd7f499ec9d
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2024-10-09 11:31:40 +0530

    Time: 39 ms (73.23%) | Memory: 58.7 MB (96.29%) - LeetSync

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int i=0 , j = 0, k= 1;
+
+        int m = INT_MIN;
+        int count = 0;
+
+        while(j < nums.size()){
+            if(nums[j] == 0){
+                k--;
+            }
+
+            if(k < 0){
+                if(nums[i] == 0){
+                    k++;
+                }
+                i++;
+            }
+            j++;
+        }
+
+        return j-i-1;
+    }
+};
