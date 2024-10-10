43f876a884062390d118858c6157f9109d9d4931
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2024-10-10 20:46:30 +0530

    Time: 9 ms (59.68%) | Memory: 20.9 MB (46.14%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -4,31 +4,27 @@ public:
         stack<int> s;
 
         for(int i=0;i<asteroids.size();i++){
-            if(s.empty()){
-                s.push(asteroids[i]);
-            }else{
-                if((asteroids[i] > 0 && s.top() > 0) || (asteroids[i] >  0 && s.top() < 0) || (asteroids[i] < 0 && s.top() < 0)){
-                    s.push(asteroids[i]);
-                }else{
-                    if(asteroids[i] < 0){
-                        bool a = false;
-                        while(!s.empty() &&s.top() > 0 ) {
-                            if(abs(asteroids[i]) < abs(s.top())){
-                                a = false;
-                                break;
-                            }else if(abs(asteroids[i]) == abs(s.top())){
-                                s.pop();
-                                a = false;
-                                break;
-                            }else{
-                                s.pop();
-                                a = true;
-                            }
-                        }
-                        if(a) s.push(asteroids[i]);
+
+            if(asteroids[i] < 0 && !s.empty() && s.top() > 0){
+                bool a = false;
+                while(!s.empty() &&s.top() > 0 ) {
+                    if(abs(asteroids[i]) < abs(s.top())){
+                        a = false;
+                        break;
+                    }else if(abs(asteroids[i]) == abs(s.top())){
+                        s.pop();
+                        a = false;
+                        break;
+                    }else{
+                        s.pop();
+                        a = true;
                     }
                 }
+                if(a) s.push(asteroids[i]);
+            }else{
+                s.push(asteroids[i]);
             }
+            
         }
 
         vector<int> ans;
