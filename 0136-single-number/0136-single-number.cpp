67e5b2906a9f511fcd306f19653f7d4c9877d19f
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2025-05-07 13:23:14 +0530

    Time: 0 ms (100%), Space: 20.5 MB (83.85%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
-
-        for(int i=0;i < nums.size()-1;i+=2){
-            if(nums[i] != nums[i+1]){
-                return nums[i];
-            }
+        int n = 0;
+        for(int i=0;i < nums.size();i++){
+            n ^= nums[i];
         }
 
-        return nums[nums.size()-1];
+        return n;
     }
 };
