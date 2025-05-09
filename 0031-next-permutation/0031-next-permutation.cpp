36503b9fc0c472b93639c49919f176cc8903f7a0
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2025-05-09 10:34:40 +0530

    Time: 0 ms (100%), Space: 15.6 MB (92.29%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -4,13 +4,13 @@ public:
        bool flag = false; 
         for(int i=nums.size()-2;i >=0 ;i--){
             if(nums[i] < nums[i+1]){
-                sort(nums.begin() + i + 1 , nums.end());
-                for(int j=i+1;j<nums.size();j++){
+                for(int j=nums.size()-1;j>i;j--){
                     if(nums[j] > nums[i]){
                         swap(nums[i] , nums[j]);
                         break;
                     }
                 }
+                reverse(nums.begin() + i +1 , nums.end());
                 flag = true;
                 break;
             }
