abb711b1cd0e05cefaa5088d92aaf36038ca26a0
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2025-05-10 13:00:55 +0530

    Time: 3 ms (15.32%), Space: 17.9 MB (21.97%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -8,18 +8,14 @@ public:
         int min_product = nums[0];
         int result = nums[0];
 
-        // Iterate over the array starting from index 1
         for (int i = 1; i < nums.size(); i++) {
-            // If the current number is negative, swap max and min product
             if (nums[i] < 0) {
                 swap(max_product, min_product);
             }
 
-            // Update the max and min product at index i
             max_product = max(nums[i], max_product * nums[i]);
             min_product = min(nums[i], min_product * nums[i]);
 
-            // Update the global result
             result = max(result, max_product);
         }
 
