4452de4d8f0a889c92e1c7c21cec97c23c5b417f
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2025-05-27 17:19:51 +0530

    Time: 87 ms (10.3%), Space: 72.4 MB (6.09%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -7,14 +7,16 @@ public:
         }
         if(target < 0 || i == n) return;
 
-        f(nums,target,temp,ans,i+1,n);
         temp.push_back(nums[i]);
         f(nums,target - nums[i],temp,ans,i,n);
+        temp.pop_back();
+        f(nums,target,temp,ans,i+1,n);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         int n = candidates.size();
         vector<vector<int>> ans;
-        f(candidates, target, {}, ans, 0,n);
+        vector<int> temp;
+        f(candidates, target, temp, ans, 0,n);
         return ans;
     }
 };
