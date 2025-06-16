978cf3f91f42c2f9e9cb30b932ce406256ed2dbb
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2025-06-16 15:42:27 +0530

    Time: 28 ms (27.04%), Space: 70.6 MB (15.94%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,49 +1,29 @@
 class Solution {
 public:
     int minDays(vector<int>& bloomDay, int m, int k) {
-        long long left = INT_MAX, right = INT_MIN;
+        int low = *min_element(bloomDay.begin(), bloomDay.end());
+        int high = *max_element(bloomDay.begin(), bloomDay.end());
+        int ans = -1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
 
-        for(auto it : bloomDay){
-            if(it > right){
-                right = it;
-            }
-
-            if(it < left){
-                left = it;
-            }
-        }
-
-        right *= m;
-
-        int ans  = -1;
-
-        while(left <= right){
-            long long mid = left + (right - left)/2;
-
+            int sum = 0;
             int count = 0;
-            int boq = 0;
-
             for(auto it : bloomDay){
-                if(count >= k){
-                    boq++;
-                    count = 0;
-                }
                 if(it <= mid){
                     count++;
-
                 }else{
+                    sum += count/k;
                     count = 0;
                 }
             }
-            if(count >= k){
-                boq++;
-            }
+            sum += count/k;
 
-            if(boq >= m){
+            if(sum >= m){
                 ans = mid;
-                right = mid - 1;
+                high = mid - 1;
             }else{
-                left = mid + 1;
+                low = mid + 1;
             }
         }
 
