4441a98e77c67059a163475f4d2b691c9e27d408
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2025-06-20 16:08:42 +0530

    Time: 479 ms (47.05%), Space: 12.9 MB (9.35%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
-    bool f(vector<vector<char>>& board, string& word, int i, int j, int& m, int& n, int k, int& len,vector<vector<bool>>& visited){
-        if(k == len){
+    bool f(vector<vector<char>>& board, int i ,int j, string& word, int ind, map<pair<int,int>, bool>& mp){
+        if(ind == word.size()){
             return true;
         }
-        if(i < 0 || j < 0 || i >= m || j >= n){
-            return false;
+
+        mp[{i,j}] = true;
+        bool up = false,left = false,right = false,down = false;
+        if(i - 1 >= 0 && board[i-1][j] == word[ind] && !mp[{i-1,j}]){
+             up =  f(board, i - 1, j, word, ind + 1 , mp);
         }
 
-
-        if(board[i][j] == word[k] && visited[i][j] != true){
-            visited[i][j] = true;
-            bool a = f(board,word,i-1,j,m,n,k+1,len,visited);
-            bool b = f(board,word,i+1,j,m,n,k+1,len,visited);
-            bool c = f(board,word,i,j-1,m,n,k+1,len,visited);
-            bool d = f(board,word,i,j+1,m,n,k+1,len,visited);
-
-            visited[i][j] = false;
-
-            if(a || b || c || d){
-                return true;
-            }
-
+        if(j - 1 >= 0 && board[i][j-1] == word[ind] && !mp[{i,j-1}]){
+             left = f(board, i, j-1, word, ind + 1, mp);
         }
+
+        if(i + 1 < board.size() && board[i+1][j] == word[ind] && !mp[{i + 1,j}]){
+             down = f(board, i+1, j, word, ind + 1, mp);
+        }
+
+        if(j + 1 < board[0].size() && board[i][j+1] == word[ind] && !mp[{i,j + 1}]){
+             right = f(board, i, j+1, word, ind + 1, mp);
+        }
+        mp[{i,j}] = false;
+
+        if(up || left || down || right){
+            return true;
+        }
+
 
         return false;
-
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        int k = word.size();
-        vector<vector<bool>> visited(m,vector<bool>(n,false));
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        map<pair<int,int>, bool> mp;
+        for(int i=0; i<board.size(); i++){
+            for(int j=0; j<board[0].size(); j++){
                 if(board[i][j] == word[0]){
-                   bool ans = f(board,word,i,j,m,n,0,k,visited);
-                   if(ans) return true;
+                    bool ans = f(board,i,j,word,1,mp);
+                    if(ans) return true;
                 }
             }
         }
 
         return false;
-
     }
 };
