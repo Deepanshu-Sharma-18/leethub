e868b2ec668e55467151fb27616de0e12b6dae95
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2025-06-29 13:51:02 +0530

    Time: 0 ms (100%), Space: 87.3 MB (39.62%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        vector<int> m(4,0);
+        int fives = 0;
+        int tens = 0;
 
         for(auto it : bills){
-            m[(it / 5) - 1]++;
-            if(it != 5){
-                int amt = it - 5;
-                for(int i = 1; i>=0; i--){
-                    int val = (i + 1) * 5;
-                    int use = min(amt / val, m[i]);
-                    amt -= use * val;
-                    m[i] -= use;
+            if(it == 5) fives++;
+            else if(it == 10){
+                tens++;
+                if(fives < 1) return false;
+                fives--;
+            }else{
+                if(tens >= 1){
+                    tens -= 1;
+                    if(fives < 1) return false;
+                    fives--;
+                }else{
+                    if(fives < 3) return false;
+                    fives -= 3;
                 }
-
-                if(amt != 0) return false;
             }
         }
 
