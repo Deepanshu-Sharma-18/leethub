ac0097b6c586265f2a82707158991cced327bacf
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2025-07-04 15:47:16 +0530

    Time: 1 ms (52.91%), Space: 20 MB (53.62%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -5,31 +5,21 @@ public:
         vector<int> vis(n,0);
         int count = 0;
 
-        vector<vector<int>> adj(n);
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(isConnected[i][j] == 1 && i != j){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
-        }
-
         for(int i=0; i<n; i++){
             if(!vis[i]){
                 queue<int> q;
                 count++;
                 q.push(i);
                 vis[i] = 1;
+
                 while(!q.empty()){
                     auto it = q.front();
                     q.pop();
 
-                    for(auto node : adj[it]){
-                        if(!vis[node]){
-                            q.push(node);
-                            vis[node] = 1;
+                    for(int j=0; j<n; j++){
+                        if(isConnected[it][j] == 1 && !vis[j]){
+                            q.push(j);
+                            vis[j] =1;
                         }
                     }
                 }
