21690c9c8b18972c1884baf4fd40bf4c9beed760
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2025-08-14 13:30:18 +0530

    Time: 0 ms (100%), Space: 18.8 MB (23.47%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,25 +1,50 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& num) {
-        vector<int> row(num.size(), 1);
-        vector<int> col(num[0].size(), 1);
+        int firstRow = 1;
+        int firstCol = 1;
 
+        for(int i=0; i<num[0].size(); i++){
+            if(num[0][i] == 0){
+                firstRow = 0;
+                break;
+            } 
+        }
 
         for(int i=0; i<num.size(); i++){
-            for(int j=0; j<num[0].size(); j++){
+            if(num[i][0] == 0){
+                firstCol = 0;
+                break;
+            } 
+        }
+
+        for(int i=1; i<num.size(); i++){
+            for(int j=1; j<num[0].size(); j++){
                 if(num[i][j] == 0){
-                    row[i] = 0;
-                    col[j] = 0;
+                    num[i][0] = 0;
+                    num[0][j] = 0;
                 }
             }
         }
 
-        for(int i=0; i<num.size(); i++){
-            for(int j=0; j<num[0].size(); j++){
-                if(row[i] == 0 || col[j] == 0){
+        for(int i=1; i<num.size(); i++){
+            for(int j=1; j<num[0].size(); j++){
+                if(num[i][0] == 0 || num[0][j] == 0){
                     num[i][j] = 0;
                 }
             }
+        }
+
+        if(firstRow == 0){
+           for(int i=0; i<num[0].size(); i++){
+                num[0][i] = 0;  
+            }
+        }
+
+        if(firstCol == 0){
+            for(int i=0; i<num.size(); i++){
+                num[i][0] = 0;
+            } 
         }
     }
 };
