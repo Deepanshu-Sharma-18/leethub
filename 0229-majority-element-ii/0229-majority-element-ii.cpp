6fae50ac7d96692a17693d79bf147f361ddb8ece
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2025-08-15 19:25:37 +0530

    Time: 0 ms (100%), Space: 23.6 MB (80.39%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,48 +1,45 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-          int majority1 = 0;
-        int majority2 = 0;
-        int count1 = 0;
-        int count2 = 0;
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
 
-        for (const int num : nums) {
-            if (num == majority1) {
+        // Phase 1: Find potential candidates
+        for (int num : nums) {
+            if (num == candidate1) {
                 count1++;
-            } else if (num == majority2) {
-                count2++;
-            } else if (count1 == 0) {
-                majority1 = num;
-                count1++;
-            } else if (count2 == 0) {
-                majority2 = num;
-                count2++;
-            } else {
-                count1--;
-                count2--;
+                continue;
             }
-        }
-
-        count1 = 0;
-        count2 = 0;
-
-        for (const int num : nums) {
-            if (num == majority1) {
-                count1++;
-            } else if (num == majority2) {
+            if (num == candidate2) {
                 count2++;
+                continue;
             }
+            if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+                continue;
+            }
+            if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+                continue;
+            }
+            count1--;
+            count2--;
         }
 
-        std::vector<int> res;
-
-        if (count1 > nums.size() / 3) {
-            res.push_back(majority1);
-        }
-        if (count2 > nums.size() / 3) {
-            res.push_back(majority2);
+        // Phase 2: Verify the candidates
+        count1 = count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
         }
 
-        return res;  
+        vector<int> res;
+        int n = nums.size();
+        if (count1 > n / 3) res.push_back(candidate1);
+        if (count2 > n / 3) res.push_back(candidate2);
+
+        return res;
     }
 };
