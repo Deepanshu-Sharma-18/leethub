54d3ea2e04709af07e9cb4a2fc6e1c2694cfc4da
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2025-11-10 22:08:24 +0530

    Time: 0 ms (100%), Space: 9.5 MB (49.28%) - LeetHub

--- 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.cpp
+++ 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     int secondHighest(string s) {
-        int arr[10] = {0};
-        for(auto it : s){
+        // int arr[10] = {0};
+        // for(auto it : s){
+        //     if(it >= '0' && it <= '9'){
+        //         arr[it -'0']++;
+        //     }
+        // }
+
+        // bool f = false;
+        // for(int i=9; i>=0; i--){
+        //     if(arr[i] > 0){
+        //         if(f) return i;
+        //         f = true;
+        //     }
+        // }
+        // return -1;
+        int fm = -1, sm = -1;
+         for(auto it : s){
             if(it >= '0' && it <= '9'){
-                arr[it -'0']++;
+                if(it - '0' < fm){
+                    sm = max(sm , it - '0');
+                }else if(it - '0' > fm){
+                    sm = fm;
+                    fm = it - '0';
+                }
             }
         }
 
-        bool f = false;
-        for(int i=9; i>=0; i--){
-            if(arr[i] > 0){
-                if(f) return i;
-                f = true;
-            }
-        }
-        return -1;
+        return sm;
     }
 };
