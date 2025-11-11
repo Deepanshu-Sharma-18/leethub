4ddffc0f594de3000d25d3ebafacd18e3670317e
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2025-11-11 21:58:06 +0530

    Time: 0 ms (100%), Space: 10.8 MB (6.09%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string t = "";
+        for(auto it : s){
+            if((it >= 'a' && it <= 'z') || ( it >= 'A' && it <= 'Z' ) || (it >= '0' && it <= '9')){
+                if(it >= 'A' && it <= 'Z'){
+                    t += ((it - 'A') + 'a'); 
+                }else{
+                    t += it;
+                }
+            }
+        }
+
+        string tmp = t;
+        reverse(t.begin(), t.end());
+
+        if(t == tmp) return true;
+        return false;
+    }
+};
