917bfd12998f3e3527a14476e6bbe729a3c6a659
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2025-12-07 10:11:44 +0530

    Time: 99 ms (13.09%), Space: 92.5 MB (19.13%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i] == nums[i-1]) return true;
+        map<int,int> mp;
+        for(auto it : nums){
+            mp[it]++;
+            if(mp[it] > 1) return true;
         }
 
         return false;
-
-        // map<int,int> mp;
-        // for(auto it : nums){
-        //     mp[it]++;
-        //     if(mp[it] > 1) return true;
-        // }
-
-        // return false;
     }
 };
