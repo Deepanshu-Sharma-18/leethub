956477a628dab84c274cb76b516d04a7f4d3f217
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2025-12-07 18:10:14 +0530

    Time: 4 ms (37.38%), Space: 9.7 MB (73.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,8 +3,8 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return false;
 
-        sort(s.begin() , s.end());
-        sort(t.begin() , t.end());
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
 
         return s == t;
     }
