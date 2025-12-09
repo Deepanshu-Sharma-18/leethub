65db501a11fdfcdf2539941730990274e964cbe4
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2025-12-09 22:40:33 +0530

    Time: 63 ms (14.48%), Space: 30.9 MB (17.53%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for (auto &s : strs) {
+            int freq[26] = {0};
+
+            for (char c : s) {
+                freq[c - 'a']++;
+            }
+
+            // convert freq array into a unique string key
+            string key;
+            for (int i = 0; i < 26; i++) {
+                key += to_string(freq[i]) + "#";
+            }
+
+            mp[key].push_back(s);
+        }
+
         vector<vector<string>> ans;
-        unordered_set<int> st;
-
-        for(int i=0; i<strs.size(); i++){
-            if(st.count(i) > 0) continue;
-            vector<string> t;
-            t.push_back(strs[i]);
-            int n1 = strs[i].length();
-            int c1[26] = {0};
-            for(auto it : strs[i]){
-                c1[it - 'a']++;
-            }
-            for(int j=i+1; j<strs.size(); j++){
-                if(st.count(j) > 0) continue;
-                int n2 = strs[j].length();
-                if(n1 != n2) continue;
-                int c2[26] = {0};
-                for(auto it : strs[j]){
-                    c2[it - 'a']++;
-                }
-
-                int cmp = true;
-                for(int i=0; i<26; i++){
-                    if(c1[i] != c2[i]){
-                        cmp = false;
-                        break;
-                    }
-                }
-                if(cmp){
-                    t.push_back(strs[j]);
-                    st.insert(j);
-                }
-            }
-
-            ans.push_back(t);
+        for (auto &p : mp) {
+            ans.push_back(p.second);
         }
 
         return ans;
     }
-};  
+};
