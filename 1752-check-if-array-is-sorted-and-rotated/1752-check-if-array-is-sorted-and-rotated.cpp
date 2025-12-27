fd71d8ca18948e22183a4866e0041a18d4e7f416
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2025-12-27 13:34:48 +0530

    Time: 0 ms (100%), Space: 11.3 MB (13.74%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        // int k = 0;
-        // for(int i=0; i<nums.size(); i++){
-        //     if(nums[k] > nums[i]) k = i;
-        // }
-
-        // for(int i=1; i<nums.size(); i++){
-        //     if(nums[(i + k)%nums.size()] < nums[(i-1+k)%nums.size()]) return false;
-        // }
-
-        // return true;
-
-        int i=1;
-        while(i < nums.size()){
-            if(nums[i] >= nums[i-1]) i++;
+        int k = 1;
+       while(k < nums.size()){
+            if(nums[k] >= nums[k-1]) k++;
             else break;
         }
-        if(i == nums.size()) return true;
-        i++;
 
-        while(i < nums.size()){
-            if(nums[i] >= nums[i-1]) i++;
-            else return false;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[(i + k)%nums.size()] < nums[(i-1+k)%nums.size()]) return false;
         }
 
-        i--;
-        if(nums[i] > nums[0]) return false;
         return true;
+
+        // int i=1;
+        // while(i < nums.size()){
+        //     if(nums[i] >= nums[i-1]) i++;
+        //     else break;
+        // }
+        // if(i == nums.size()) return true;
+        // i++;
+
+        // while(i < nums.size()){
+        //     if(nums[i] >= nums[i-1]) i++;
+        //     else return false;
+        // }
+
+        // i--;
+        // if(nums[i] > nums[0]) return false;
+        // return true;
     }
 };
